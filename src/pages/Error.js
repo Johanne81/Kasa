d5370b27a438add698e9210f4a6f3aaa2e09f67a
johanne <joseznec@hotmail.fr>
2024-03-18 16:10:54 +0100

    Add style for Error page

--- src/pages/Error.js
+++ src/pages/Error.js
@@ -1,19 +1,21 @@
 import React from "react";
 import Header from "../components/Header";
 import Footer from "../components/Footer";
-import { NavLink } from "react-router-dom";
+import { Link } from "react-router-dom";
 
 const Error = () => {
   return (
-    <div>
+    <>
       <Header />
-      404
-      <p>Oups! La page que vous demandez n'existe pas.</p>
-      <NavLink to="/">
-        <p>Retourner sur la page d'accueil</p>
-      </NavLink>
+      <main className="error">
+        <h2>404</h2>
+        <p>Oups! La page que vous demandez n'existe pas.</p>
+        <Link className="error-link" to="/">
+          Retourner sur la page d'accueil
+        </Link>
+      </main>
       <Footer />
-    </div>
+    </>
   );
 };
 
